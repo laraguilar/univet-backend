6230a784f52f72d48eca6e44033ba966aa834e47
Lara Aguilar <72893552+laraguilar@users.noreply.github.com>
2025-02-13 09:19:33 -0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,34 +4,20 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
-import * as fs from 'fs'
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('/etc/letsencrypt/live/seu-dominio/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/seu-dominio/fullchain.pem'),
-  }
-
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ https: httpsOptions }),
+    new FastifyAdapter(),
   )
 
-  // Configuração CORS
   app.enableCors({
-    origin: true,
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD'],
-    allowedHeaders: [
-      'Content-Type',
-      'Authorization',
-      'Accept',
-      'Origin',
-      'X-Requested-With',
-    ],
-    exposedHeaders: ['Content-Range', 'X-Content-Range'],
-    credentials: true,
-    maxAge: 3600,
+    origin: '*', // Permite qualquer origem (não recomendado para produção)
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true, // Se estiver lidando com cookies/sessões
   })
-  await app.listen(3001, '0.0.0.0')
+
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0')
 }
 bootstrap()
