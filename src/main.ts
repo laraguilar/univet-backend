3a10bc8dbf2d38fc170a2f28194f9803a95ca8e1
Lara Aguilar <72893552+laraguilar@users.noreply.github.com>
2025-02-13 09:17:11 -0300

    updatye main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,37 +7,31 @@ import { AppModule } from './app.module'
 import * as fs from 'fs'
 
 async function bootstrap() {
-  // const httpsOptions = {
-  //   key: fs.readFileSync('path/to/private-key.pem'),
-  //   cert: fs.readFileSync('path/to/certificate.pem'),
-  // }
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/live/seu-dominio/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/seu-dominio/fullchain.pem'),
+  }
 
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    // new FastifyAdapter({
-    //   logger: true,
-    //   https: httpsOptions,
-    // }),
-    new FastifyAdapter(),
-    {
-      cors: {
-        origin: '*',
-        methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-        allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-        credentials: true,
-      },
-    },
+    new FastifyAdapter({ https: httpsOptions }),
   )
 
-  // app.enableCors({
-  //   origin: true,
-  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-  //   allowedHeaders: ['Content-Type', 'Authorization'],
-  //   credentials: true,
-  // })
-
-  const port = process.env.PORT ?? 3000
-  await app.listen(port, '0.0.0.0')
-  console.log(`Application is running on: ${await app.getUrl()}`)
+  // Configuração CORS
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS', 'PATCH', 'HEAD'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'Origin',
+      'X-Requested-With',
+    ],
+    exposedHeaders: ['Content-Range', 'X-Content-Range'],
+    credentials: true,
+    maxAge: 3600,
+  })
+  await app.listen(3001, '0.0.0.0')
 }
 bootstrap()
