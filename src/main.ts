eb6d534f4cb25b2bd887c6337555f2616be9fcba
Lara Aguilar <72893552+laraguilar@users.noreply.github.com>
2025-02-13 08:59:40 -0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,37 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
+import * as fs from 'fs'
 
 async function bootstrap() {
+  // const httpsOptions = {
+  //   key: fs.readFileSync('path/to/private-key.pem'),
+  //   cert: fs.readFileSync('path/to/certificate.pem'),
+  // }
+
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({
-      logger: true, // Adiciona logging para debugar
-    }),
+    // new FastifyAdapter({
+    //   logger: true,
+    //   https: httpsOptions,
+    // }),
+    new FastifyAdapter(),
+    {
+      cors: {
+        origin: '*',
+        methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+        credentials: true,
+      },
+    },
   )
 
-  app.enableCors({
-    origin: true, // Mude para true para aceitar a origem da requisição
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  })
+  // app.enableCors({
+  //   origin: true,
+  //   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: true,
+  // })
 
   const port = process.env.PORT ?? 3000
   await app.listen(port, '0.0.0.0')
